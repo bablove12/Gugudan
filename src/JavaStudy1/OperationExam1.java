7a0b8143d38fee3a4a53351201baff464b636b00
bablo <bablo@DESKTOP-9EEPDAA>
2018-05-12 03:58:32 +0900

    강의추가

--- src/JavaStudy1/OperationExam1.java
+++ src/JavaStudy1/OperationExam1.java
@@ -3,7 +3,22 @@ package JavaStudy1;
 public class OperationExam1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int i = 10;
+		int j = 10;
+		
+		System.out.println(i == j);
+		System.out.println(i != j);
+		System.out.println(i < j);
+		System.out.println(i <= j);
+		System.out.println(i > j);
+		System.out.println(i >= j);
+		
+		i += 10; //i = i + 10;
+		System.out.println(i);
+		
+		System.out.println(i -=5); //i = i -5;
+		System.out.println(i);
+		
 
 	}
 
