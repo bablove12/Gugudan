200f884ee34fecfb5f20da4c1d9d559c1f629c5f
bablo <bablo@DESKTOP-9EEPDAA>
2018-05-12 04:49:52 +0900

    강의 저장

---
+++ src/JavaStudy1/OperationExam2.java
@@ -0,0 +1,19 @@
+package JavaStudy1;
+
+public class OperationExam2 {
+
+	public static void main(String[] args) {
+		int a = 5;
+		int b = 10;
+		int c = 15;
+		
+		System.out.println(a - b * c); //-145
+		System.out.println((a - b) * c); //-75
+		System.out.println(a > 5 && b > 5); //true and false
+		System.out.println(a > 5 || b > 5); //true or true;
+		System.out.println(a++ -5); //0
+		
+		System.out.println(a); //6
+	}
+
+}
