c27981646e19f89d83c91b8b08543544b46755ee
bablo <bablo@DESKTOP-9EEPDAA>
2018-05-12 19:10:10 +0900

    강의

---
+++ src/Chapter3/Switch.java
@@ -0,0 +1,22 @@
+package Chapter3;
+
+public class Switch {
+
+	public static void main(String[] args) {
+		int value = 1;
+		
+		switch(value) {
+		case 1 :
+			System.out.println("1");
+		case 2 : 
+			System.out.println("2");
+		case 3 :
+			System.out.println("3");
+		default :
+			System.out.println("그외 다른 숫자");
+		
+		}
+
+	}
+
+}
